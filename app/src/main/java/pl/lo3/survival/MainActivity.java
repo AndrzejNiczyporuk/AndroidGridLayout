e0b454df5fd946d1ab54125fc82f4d965609a716
Andrzej.Niczyporuk <anicz2000@gmail.com>
2018-04-30 13:33:33 +0200

    Otwieranie pdf pod przyciskiem na podstawie https://deepshikhapuri.wordpress.com/2017/02/15/display-pdf-file-inside-my-android-application/

--- app/src/main/java/pl/lo3/survival/MainActivity.java
+++ app/src/main/java/pl/lo3/survival/MainActivity.java
@@ -1,13 +1,11 @@
 package pl.lo3.survival;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.GridLayout;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,34 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainGrid = (GridLayout) findViewById(R.id.mainGrid);
-
-        //Set Event
         setSingleEvent(mainGrid);
-        //setToggleEvent(mainGrid);
+
     }
 
-    private void setToggleEvent(GridLayout mainGrid) {
-        //Loop all child item of Main Grid
-        for (int i = 0; i < mainGrid.getChildCount(); i++) {
-            //You can see , all child item is CardView , so we just cast object to CardView
-            final CardView cardView = (CardView) mainGrid.getChildAt(i);
-            cardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
-                        Toast.makeText(MainActivity.this, "State : True", Toast.LENGTH_SHORT).show();
-
-                    } else {
-                        //Change background color
-                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
-                        Toast.makeText(MainActivity.this, "State : False", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
-    }
 
     private void setSingleEvent(GridLayout mainGrid) {
         //Loop all child item of Main Grid
@@ -58,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
 
-                    Intent intent = new Intent(MainActivity.this,ActivityOne.class);
-                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                   // Intent intent = new Intent(MainActivity.this,ActivityOne.class);
+                    Intent intent = new Intent(MainActivity.this,ActivityPDF.class);
+                    intent.putExtra("info",""+finalI);
                     startActivity(intent);
 
                 }
